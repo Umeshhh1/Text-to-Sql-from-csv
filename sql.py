96e2df079644e9772a683df6702aef118987cc3c
Umeshhh1 <135005378+Umeshhh1@users.noreply.github.com>
2026-02-02 10:24:34 +0530

    Updated from 1.5 to 3 flash

--- sql.py
+++ sql.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyB5e3pbr3bZKY0gr7GhWOULZuafoHIliRk")
 
-model = genai.GenerativeModel(model_name="gemini-3-flash")
+model = genai.GenerativeModel(model_name="gemini-flash-latest")
 def generate_sql(prompt, schema_description):
     full_prompt = f"""
 You are an AI assistant that converts natural language into SQL queries.
